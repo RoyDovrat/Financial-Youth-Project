65b945c1d1592559356314104f68517de1e22af9
RoyDovrat <106964190+RoyDovrat@users.noreply.github.com>
2025-01-10 17:23:09 +0200

    finish stage1

--- client/src/pages/Stage1.tsx
+++ client/src/pages/Stage1.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDrop } from 'react-dnd';
 import { useNavigate } from 'react-router-dom';
+import { MouseEvent, useRef } from "react";
 import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -13,6 +14,7 @@ import additionalData from '../images/additional data.png';
 import salary from '../images/salary.png';
 import deductionsAllowed from '../images/deductions allowed.png';
 import payment from '../images/payment.png';
+import victory from '../images/victory.avif';
 
 
 const STAGE_NUMBER = 1
@@ -26,18 +28,17 @@ type WordItem = {
 
 const INITIAL_WORDS_LIST: WordItem[] = [
   { id: 1, value: 'פרטי עובד ומעביד' },
-  { id: 2, value: 'תאריך' },
-  { id: 3, value: 'לכבוד' },
-  { id: 4, value: 'שם מלא עובד' },
-  { id: 5, value: 'כתובת' },
-  { id: 6, value: 'פרטים אישיים' },
-  { id: 7, value: 'ת"ז עובד' },
+  { id: 2, value: 'נתונים נוספים' },
+  { id: 3, value: 'ניכויים (חובה)' },
+  { id: 4, value: 'תשלומים (שכר והחזרי הוצאות)' },
+  { id: 5, value: 'התשלום' },
+  { id: 6, value: 'ניכויים (רשות)' },
 ];
 
 function Stage1() {
   const [wordBank, setWordBank] = useState<WordItem[]>(INITIAL_WORDS_LIST);
-  const [slots, setSlots] = useState<(WordItem | null)[]>([null]);
-  //const navigate = useNavigate();
+  const [slots, setSlots] = useState<(WordItem | null)[]>([null, null, null, null, null, null]);
+  const navigate = useNavigate();
 
   const createDrop = (index: number) =>
     useDrop<
@@ -63,26 +64,52 @@ function Stage1() {
       );
     }
   };
-/*
-  const handleNextStage = () => {
-    navigate('/stage2'); // Navigate to the next stage
+
+  //pop-up
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const open = () => {
+    dialogRef.current?.showModal();
+    //dialogRef.current?.show();
   };
-*/
+  const closeAndNextStage = () => {
+    navigate('/stage2');
+
+  };
+
+  const closeAndCurrentStage = () => {
+    dialogRef.current?.close();
+    setSlots([null])
+    setWordBank(INITIAL_WORDS_LIST)
+  };
+
+
+  const dialogClick = (e: MouseEvent) => {
+    if ((e.target as HTMLDialogElement).tagName == "DIALOG") {
+      close();
+    }
+  };
 
   return (
     <div className='page-stage1'>
-      <div className='instructions'>
-        <h1 className='title'>השלימו את התלוש</h1>
-        <p>בחרו מילה מתוך בנק המילים</p>
-        <p>והתאימו אותם למיקום הנכון בתלוש השכר</p>
+      <div >
+        <h1 className='title'>להשלים את התלוש</h1>
+        <p className='instructions'>בחרו מילה מתוך בנק המילים</p>
+        <p className='instructions'>והתאימו אותם למיקום הנכון בתלוש השכר</p>
       </div>
 
       {/* progress bar */}
-      <Box sx={{ width: '100%', margin: '30px 0' }}>
-        <Typography variant="body2" component="div" align="left" gutterBottom>
+      <Box sx={{ width: '100%', margin: '20px 0' }}>
+        <Typography variant="body2" component="div" align="left" gutterBottom sx={{ fontWeight: 'bold', color: '#54177cf2' }}>
           {STAGE_NUMBER} / {TOTAL_STAGES}
         </Typography>
-        <LinearProgress variant="determinate" value={PROGRESS} />
+        <LinearProgress variant="determinate" value={PROGRESS}
+          sx={{
+            '& .MuiLinearProgress-bar': {
+              backgroundColor: '#54177cf2',
+            },
+            backgroundColor: '#d3c0e3f2', // background of the track
+          }} />
       </Box>
 
       <div className="payslip-container">
@@ -108,11 +135,10 @@ function Stage1() {
                 className={`payslip-slot slot-${index}`}
 
               >
-                {slot ? (
-                  <div className="word-container">{slot.value}</div>
-                ) : (
-                  'Drop Here'
-                )}
+                {slot ? (<div className="word-container">{slot.value}</div>) :
+                  (
+                    ''
+                  )}
 
               </div>
             );
@@ -123,7 +149,7 @@ function Stage1() {
       </div>
 
       <div className="Words-bank-container">
-        <p>בנק מילים</p>
+        <p className='bank-title'>בנק מילים</p>
         <div className='words-container'>
           {wordBank.map((word) => (
             <Word key={word.id} word={word} />
@@ -132,9 +158,29 @@ function Stage1() {
 
       </div>
 
-    {/*  <button className="next-stage-button" onClick={handleNextStage}>
+      {/*  <button className="next-stage-button" onClick={handleNextStage}>
         השלב הבא
       </button>*/}
+
+      {/*pop-up */}
+      <dialog ref={dialogRef} onClick={dialogClick}>
+        <div className="dialog-content">
+          
+          <div className="dialog-title">!אליפות</div>
+          <p className="dialog-message">עברת את השלב</p>
+          
+            <img src={victory} alt="victory" className="victory-image" />
+        
+
+          <div className="buttons">
+            <button className='nextStage-button' onClick={closeAndNextStage}>שלב הבא</button>
+            <button className='currentStage-button' onClick={closeAndCurrentStage}>שחק שוב</button>
+          </div>
+        </div>
+
+      </dialog>
+
+      <button className="next-stage-button" onClick={open} disabled={slots.some((slot) => slot === null)}>שלב הבא</button>
     </div>
   );
 }
