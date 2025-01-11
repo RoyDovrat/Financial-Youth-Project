75ae8591ec3ab1e0b1c339358dc0f27782e50e43
RoyDovrat <106964190+RoyDovrat@users.noreply.github.com>
2025-01-11 18:32:06 +0200

    s3

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,26 +1,58 @@
-const uuid = require('uuid').v4 
+const uuid = require('uuid').v4
 const multer = require('multer');
 const path = require('path');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
+/*
 const storage = multer.diskStorage({
-  destination: 'uploads/', // temporary directory
-  filename: (req, file, cb) => {
-    const { originalname } = file;
-    const ext = path.extname(originalname); // Get file extension
-    cb(null, `${uuid()}${ext}`); 
-  },
+    destination: 'uploads/', // temporary directory
+    filename: (req, file, cb) => {
+        const { originalname } = file;
+        const ext = path.extname(originalname); // get file extension
+        cb(null, `${uuid()}${ext}`);
+    },
 });
+*/
 
-const upload = multer({
-  storage, 
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB limit
-  fileFilter: (req, file, cb) => {
-    const ext = path.extname(file.originalname).toLowerCase(); 
+  const s3Uploadv3 = async (files) => {
+  const s3client = new S3Client({ region: process.env.AWS_REGION });
+
+  const params = files.map((file) => {
+    console.log("Uploading file:", file.originalname); 
+    return {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `uploads/${uuid()}-${file.originalname}`,
+      Body: file.buffer,
+    };
+  });
+
+  try {
+    const results = await Promise.all(
+      params.map((param) => s3client.send(new PutObjectCommand(param)))
+    );
+    console.log("Upload successful:", results);
+    return results;
+  } catch (error) {
+    console.error("S3 Upload Error:", error); // Log the error message
+    throw new Error(`S3 upload failed: ${error.message}`);
+  }
+};
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    const ext = path.extname(file.originalname).toLowerCase();
     if (ext !== '.mp4' && ext !== '.mov') {
-      return cb(new Error('Only video files are allowed'), false);
+        return cb(new Error('Only video files are allowed'), false);
     }
     cb(null, true);
-  },
+};
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 50 * 1024 * 1024 }, // 50MB limit
+
 });
 
-module.exports = upload;
+module.exports = { upload, s3Uploadv3 };
